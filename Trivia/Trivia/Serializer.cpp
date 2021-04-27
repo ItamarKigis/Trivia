5fd6bdc8ee37d73b07f48125461972f08d5faa8a
Yuval Elron <yuval.binyamini@gmail.com>
2021-04-27 19:33:37 +0300

    started writing serialize response

--- Trivia/Trivia/Serializer.cpp
+++ Trivia/Trivia/Serializer.cpp
@@ -1,9 +1,21 @@
 #include "Serializer.h"
+#include <bitset>
+using json = nlohmann::json;
 
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(ErrorResponse res)
 {
+    int i = 1;
+    std::vector<unsigned char> pack;
+    json j;
+    j["message"] = res.message;
+    pack[0] = ERROR;
+    std::string size = std::bitset<32>(sizeof(j)).to_string();
+    for (auto& let : size) {
+        pack[i] = let;
+        i++;
+    }
 
-    return std::vector<unsigned char>();
+    return pack;
 }
 
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(LoginResponse res)
