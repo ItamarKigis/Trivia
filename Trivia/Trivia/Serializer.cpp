8b815f54b59ad516c1a0f6a03ad0e3cc476c0ae8
Yuval Elron <yuval.binyamini@gmail.com>
2021-04-29 18:33:04 +0300

    finished two more versions of seralizeResponse

--- Trivia/Trivia/Serializer.cpp
+++ Trivia/Trivia/Serializer.cpp
@@ -23,10 +23,38 @@ std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(Error
 
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(LoginResponse res)
 {
-    return std::vector<unsigned char>();
+    std::vector<unsigned char> pack;
+    json j;
+    j["status"] = res.status;
+    pack[0] = LOGIN;
+    std::string size = std::bitset<32>(sizeof(j)).to_string();
+    for (unsigned char let : size)
+    {
+        pack.push_back(let);
+    }
+    std::string msg = j.dump();
+    for (unsigned char let : msg)
+    {
+        pack.push_back(let);
+    }
+    return pack;
 }
 
 std::vector<unsigned char> JsonResponsePacketSerializer::serializeResponse(SignupResponse res)
 {
-    return std::vector<unsigned char>();
+    std::vector<unsigned char> pack;
+    json j;
+    j["status"] = res.status;
+    pack[0] = SIGN_UP;
+    std::string size = std::bitset<32>(sizeof(j)).to_string();
+    for (unsigned char let : size)
+    {
+        pack.push_back(let);
+    }
+    std::string msg = j.dump();
+    for (unsigned char let : msg)
+    {
+        pack.push_back(let);
+    }
+    return pack;
 }
