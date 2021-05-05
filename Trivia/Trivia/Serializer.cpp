6159407d60db57d08f404c79fbf5b2a17b8262cc
Yuval Elron <yuval.binyamini@gmail.com>
2021-05-05 15:33:49 +0300

    changed parameters' names in one of the functions

--- Trivia/Trivia/Serializer.cpp
+++ Trivia/Trivia/Serializer.cpp
@@ -6,16 +6,16 @@ using json = nlohmann::json;
 #define FOUR_BYTES 32
 #define BYTE 8
 #define BINARY 2
-void JsonResponsePacketSerializer::pushValsToVector(std::vector<unsigned char>& pack, const std::string& jsonMsg,const std::string& json)
+void JsonResponsePacketSerializer::pushValsToVector(std::vector<unsigned char>& pack, const std::string& sizeOfData,const std::string& data)
 {
-    for (int i = 0; i < jsonMsg.length(); i += BYTE)
+    for (int i = 0; i < sizeOfData.length(); i += BYTE)
     {
-        unsigned char byte = stoi(jsonMsg.substr(i, i + BYTE), 0, BINARY);
+        unsigned char byte = stoi(sizeOfData.substr(i, i + BYTE), 0, BINARY);
         pack.push_back(byte);
     }
-    for (int i = 0; i < json.length(); i += BYTE)
+    for (int i = 0; i < data.length(); i += BYTE)
     {
-        unsigned char byte = stoi(json.substr(i, i + BYTE), 0, BINARY);
+        unsigned char byte = stoi(data.substr(i, i + BYTE), 0, BINARY);
         pack.push_back(byte);
     }
 }
--- Trivia/Trivia/Serializer.h
+++ Trivia/Trivia/Serializer.h
@@ -5,7 +5,7 @@
 enum class CODES { LOGIN = 200, SIGN_UP = 210, ERROR = 500 };
 class JsonResponsePacketSerializer
 {
-	static void pushValsToVector(std::vector<unsigned char>& pack, const std::string& size, const std::string& json);
+	static void pushValsToVector(std::vector<unsigned char>& pack, const std::string& sizeOfData, const std::string& data);
 public:
 	static std::vector<unsigned char> serializeResponse(ErrorResponse response);
 	static std::vector<unsigned char> serializeResponse(LoginResponse response);
