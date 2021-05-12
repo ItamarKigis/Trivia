e8819492734d32b0b1484927a3eb403dd70a0d78
Yuval Elron <yuval.binyamini@gmail.com>
2021-05-12 12:59:44 +0300

    dtor is now marked with override

--- Trivia/Trivia/SqliteDatabase.h
+++ Trivia/Trivia/SqliteDatabase.h
@@ -12,7 +12,7 @@ class SqliteDatabase : public IDataBase
 	static int userExistsCallback(void* data, int argc, char** argv, char** azColName);
 public:
 	SqliteDatabase(const std::string dbName);
-	~SqliteDatabase();
+	~SqliteDatabase() override;
 	virtual bool doesUserExists(const std::string) const override;
 	virtual bool doesPasswordMatch(const std::string, const std::string) const override;
 	virtual void addNewUser( const std::string,const std::string,const std::string) override;
