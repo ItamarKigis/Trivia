a55819ea83c6c133c9b9e7ff23b95b16a0fbaf22
Yuval Elron <yuval.binyamini@gmail.com>
2021-05-24 19:10:49 +0300

    fixed bugs in script

--- addQuestionsToDB.py
+++ addQuestionsToDB.py
@@ -1,23 +1,16 @@
-import requests, sqlite3
+import requests
 import sqlite3
 
 def dataToDB(db,data):
     con = sqlite3.connect(db)
     cur = con.cursor()
-    cur.execute('''CREATE TABLE QUESTIONS IF NOT EXISTS (
-                ID INT INCREMENT PRIMARY KEY,
-                DATA TEXT NOT NULL,
-                CORRECT_ANSWER TEXT NOT NULL,
-                ANS2 TEXT NOT NULL,
-                ANS3 TEXT NOT NULL,
-                ANS4 TEXT NOT NULL)''')
-    con.commit()
     for question in data['results']:
-        cur.execute("""INSERT INTO QUESTIONS (DATA,CORRECT_ANSWER,ANS2,ANS3,ANS4) VALUES (?,?,?,?,?);""",
+        incorrect = question['incorrect_answers']
+        cur.execute("""INSERT INTO QUESTIONS (DATA,CORRECT_ANSWER,ANS2,ANS3,ANS4) VALUES (?,?,?,?,?)""",
                     (question['question'], question['correct_answer'],
-                     question['incorrect_answers'][0],
-                     question['incorrect_answers'][1],
-                     question['incorrect_answers'][2]))
+                     incorrect[0],
+                     incorrect[1],
+                     incorrect[2]))
     con.commit()
     con.close()
 
@@ -27,6 +20,7 @@ def getData(url):
 
 def main():
     data = getData("https://opentdb.com/api.php?amount=10&category=11&difficulty=easy")
+    print(data)
     dataToDB("Trivia/Trivia/DataBaseFile.db",data)
 
 
