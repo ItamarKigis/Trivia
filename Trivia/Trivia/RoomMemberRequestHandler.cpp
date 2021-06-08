8909df6a0c373bc5e9a301475a3facc30f05ce2b
Yuval Elron <yuval.binyamini@gmail.com>
2021-06-08 18:11:41 +0300

    added RoomMemberHandler with its funcs

---
+++ Trivia/Trivia/RoomMemberRequestHandler.cpp
@@ -0,0 +1,46 @@
+#include "RoomMemberRequestHandler.h"
+
+RoomMemberRequestHandler::RoomMemberRequestHandler(const Room room, const LoggedUser user, RoomManager& manager, RequestHandlerFactory& handlerFactory) :
+	m_room(room), m_user(user), m_roomManager(manager), m_handlerFactory(handlerFactory)
+{
+}
+
+RoomMemberRequestHandler::~RoomMemberRequestHandler()
+{
+}
+
+bool RoomMemberRequestHandler::isRequestRelevant(RequestInfo request) const
+{
+	return request.RequestId == (int)CODES::LEAVE_ROOM_REQUEST || request.RequestId == (int)CODES::GET_ROOM_STATE_REQUEST;
+}
+
+RequestResult RoomMemberRequestHandler::handleRequest(RequestInfo request)
+{
+	if (!isRequestRelevant(request))
+	{
+		std::vector<unsigned char> response;
+		RequestResult result;
+		ErrorResponse error = { "ERROR" };
+		response = JsonResponsePacketSerializer::serializeResponse(error);
+		result.newHandler = nullptr;
+		result.response = response;
+		return result;
+	}
+	else
+	{
+		if (request.RequestId == int(CODES::GET_ROOM_STATE_REQUEST))
+			return getRoomState(request);
+		else if (request.RequestId == int(CODES::LEAVE_ROOM_REQUEST))
+			return leaveRoom(request);
+	}
+}
+
+RequestResult RoomMemberRequestHandler::leaveRoom(RequestInfo request) const
+{
+	return RequestResult();
+}
+
+RequestResult RoomMemberRequestHandler::getRoomState(RequestInfo request) const
+{
+	return RequestResult();
+}
--- Trivia/Trivia/RoomMemberRequestHandler.h
+++ Trivia/Trivia/RoomMemberRequestHandler.h
@@ -5,21 +5,17 @@
 #include "RoomManager.h"
 #include "RequestHandlerFactory.h"
 
-class RequestHandlerFactory;
-class LoginManager;
-
-class RoomAdminRequestHandler : public IRequestHandler
+class RoomMemberRequestHandler : public IRequestHandler
 {
 	Room m_room;
 	LoggedUser m_user;
 	RoomManager& m_roomManager;
 	RequestHandlerFactory& m_handlerFactory;
-	RequestResult closeRoom(RequestInfo request) const;
-	RequestResult startGame(RequestInfo request) const;
+	RequestResult leaveRoom(RequestInfo request) const;
 	RequestResult getRoomState(RequestInfo request) const;
 public:
-	RoomAdminRequestHandler(const Room room, const LoggedUser user, RoomManager& manager, RequestHandlerFactory& handlerFactory);
-	~RoomAdminRequestHandler();
+	RoomMemberRequestHandler(const Room room, const LoggedUser user, RoomManager& manager, RequestHandlerFactory& handlerFactory);
+	~RoomMemberRequestHandler();
 	bool isRequestRelevant(RequestInfo request) const override;
 	RequestResult handleRequest(RequestInfo request);
 };
