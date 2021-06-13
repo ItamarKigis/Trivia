ea68d2a2568814a817a46c34c9aba36f124e9a29
Itamar Kigis <itamarkigis04@gmail.com>
2021-06-13 20:25:48 +0300

    feat: joiRoom page added to menu

--- client/client/Menu.xaml.cs
+++ client/client/Menu.xaml.cs
@@ -49,9 +49,9 @@ namespace client
         {
             this.Control.Content = new CreateRoom();
         }
-        private void temp(object sender, MouseButtonEventArgs e)
+        private void JoinRoomClick(object sender, MouseButtonEventArgs e)
         {
-            this.Control.Content = new temp();
+            this.Control.Content = new JoinRoom();
         }
     }
 }
